321bdd984b5a858c639317eb48f8a4f862cbabad
Felipe Quintero <ffquint@gmail.com>
2020-12-13 12:00:54 -0500

    began code for header.js, console.log and other code is not appearing in brower after running npm install and npm start. Restarting VS code and npm commands.

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,25 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const entryPoint = document.querySelector('div.header-container')
+
+function Header() {
+    const headerDiv = document.createElement('div')
+    const headerDate = document.createElement('span')
+    const headerH1 = document.createElement('h1')
+    const headerTemp = document.createElement('span')
+
+    headerDiv.classList.add('header')
+    headerDate.classList.add('date')
+    headerTemp.classList.add('temp')
+
+    headerDiv.appendChild(headerDate)
+    headerDiv.appendChild(headerH1)
+    headerDiv.appendChild(headerTemp)
+
+    console.log(headerDiv)
+
+    entryPoint.appendChild(headerDiv)
+
+    return headerDiv
+}
